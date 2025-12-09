a9045ad362db5f053de617fe115673a349060119
Agape Dimas <agapedimas14@gmail.com>
2025-12-09 14:18:23 +0700

    Tambah frontend
    - Termasuk tambah jajanan
    - Belom bisa remove jajanan

---
+++ public/algorithm.js
@@ -0,0 +1,22 @@
+const items = [];
+
+/**
+ * @param { number } jumlahBingkisan
+ * @param { number } maksimalHargaPerBingkisan 
+ * @param { number } minimalJumlahMakanan 
+ * @param { number } minimalJumlahMinuman 
+ * @returns { Array<Bingkisan> }
+ */
+function buatAcak(jumlahBingkisan, maksimalHargaPerBingkisan, minimalJumlahMakanan, minimalJumlahMinuman) {
+    const daftarJajanan = dapatkanDaftarJajanan();
+
+    // buat algoritma graph coloring di sini
+
+    // dapet daftar kelompok item per warna
+
+    // randomizer menghasilkan semua kombinasi yang mungkin
+
+    // validator menghasilkan kombinasi yg valid
+
+    return //... ;
+}
---
+++ public/scripts.js
@@ -0,0 +1,172 @@
+// kelas untuk jajanan
+class Item {
+    /**
+     * @param { string } nama 
+     * @param { number } harga 
+     * @param { number } jumlah 
+     * @param { "coklat" | "stroberi" | "vanilla" | "matcha" | "mocha" | "caramel" } rasa 
+     * @param { "makanan" | "minuman" } tipe
+     */
+    constructor(nama, harga, jumlah, rasa, tipe) {
+        this.nama = nama;
+        this.harga = harga;
+        this.jumlah = jumlah;
+        this.rasa = rasa;
+        this.tipe = tipe;
+    }
+}
+
+// kelas untuk bingkisan
+class Bingkisan {
+    /**
+     * @param { Array<Item> } daftarJajanan
+     */
+    constructor(daftarJajanan) {
+        this.daftarJajanan = daftarJajanan;
+    }
+}
+
+/**
+ * Mengambil data daftar jajanan dari database
+ * @returns { void }
+ */
+function dapatkanDaftarJajanan() {
+    return JSON.parse(localStorage.getItem("jajanan")) || [];
+}
+
+/**
+ * Mengambil data daftar jajanan dari database,
+ * lalu akan mengupdate UI
+ * @returns { void }
+ */
+function tampilkanDaftarJajanan() {
+    const daftarJajanan = dapatkanDaftarJajanan();
+    
+    for (const item of daftarJajanan) {
+        const jajan = new Item(item.nama, item.harga, item.jumlah, item.rasa, item.tipe);
+        renderJajanan(jajan);
+    }
+}
+
+function renderJajanan(jajan) {
+    const nameElement = document.createElement("span");
+    nameElement.classList.add("name");
+    nameElement.innerText = jajan.nama;
+
+    const priceElement = document.createElement("span");
+    priceElement.classList.add("price");
+    priceElement.innerText = formatCurrency(jajan.harga);
+
+    const typeElement = document.createElement("span");
+    typeElement.classList.add("type");
+    typeElement.innerHTML = jajan.tipe;
+
+    const quantityElement = document.createElement("span");
+    quantityElement.classList.add("quantity");
+    quantityElement.innerHTML = jajan.jumlah;
+
+    const flavorElement = document.createElement("span");
+    flavorElement.classList.add("flavor");
+    flavorElement.innerText = jajan.rasa;
+
+    const descriptionElement = document.createElement("div");
+    descriptionElement.classList.add("description");
+    descriptionElement.append(nameElement);
+    descriptionElement.append(priceElement);
+    descriptionElement.append(typeElement);
+    descriptionElement.append(flavorElement);
+
+    const groupElement = document.createElement("div");
+    groupElement.classList.add("group");
+    groupElement.append(nameElement);
+    groupElement.append(descriptionElement);
+
+    const containerElement = document.createElement("div");
+    containerElement.classList.add("stack");
+    containerElement.append(groupElement);
+    containerElement.append(quantityElement);
+
+    List_DaftarJajanan.append(containerElement);
+}
+
+/**
+ * Menambah jajanan ke dalam database
+ * @param { Item } jajanan 
+ * @returns { boolean }
+ */
+function tambahJajanan(jajanan) {
+    let berhasil = true;
+    
+    const daftarJajanan = dapatkanDaftarJajanan();
+    daftarJajanan.push(jajanan);
+    localStorage.setItem("jajanan", JSON.stringify(daftarJajanan));
+    
+    renderJajanan(jajanan);
+    
+    return berhasil;
+}
+
+/**
+ * Menghapus jajanan dari database
+ * @param { Item } jajanan
+ * @returns { boolean } 
+ */
+function hapusJajanan(jajanan) {
+    let berhasil = true;
+    return berhasil;
+}
+
+function bukaFormJajanan() {
+    Input_Nama.value = "";
+    Input_Harga.value = formatCurrency(0);
+    Input_Jumlah.value = 1;
+    Select_Rasa.value = "coklat";
+    Select_Tipe.value = "makanan";
+    PopOver_FormJajanan.open(List_DaftarJajanan.children[0]);
+    PopOver_FormJajanan.onopened = function() {
+        Input_Nama.focus();
+    }
+}
+
+Button_BatalFormJajanan.onclick = function() {
+    PopOver_FormJajanan.close();
+}
+Button_SelesaiFormJajanan.onclick = function() {
+    const item = new Item(
+        Input_Nama.value.trim(),
+        Input_Harga.valueInt || 0,
+        parseInt(Input_Jumlah.value) || 1,
+        Select_Rasa.value,
+        Select_Tipe.value
+    )
+    tambahJajanan(item);
+    PopOver_FormJajanan.close();
+}
+registerCurrencyInput(Input_Harga);
+
+function formatCurrency(number) {
+    let result = number.toLocaleString(document.documentElement.lang, { style: "currency", currency: "IDR" });
+    result = result.replace(/IDR\s/, "Rp");
+    return result;
+}
+
+function registerCurrencyInput(input) {
+    input.addEventListener("focus", function() {
+        this.value = this.value.replace(/[^0-9|.]/g, "");
+        this.valueInt = parseInt(this.value || 0);
+        this.value = this.valueInt + "";
+    });
+
+    input.addEventListener("input", function () {
+        this.value = this.value.replace(/[^0-9]/g, "");
+        this.valueInt = parseInt(this.value);
+    });
+
+    input.addEventListener("blur", function() {
+        this.value = formatCurrency(parseInt(this.value.replace(/[^0-9|.]/g, "")) || 0);
+    });
+}
+
+window.onload = function() {
+    tampilkanDaftarJajanan();
+}
